266c4091359bd920095d8b54fd63a4902463a384
Kim, Joohong <joohongkim@kstargroup.tv>
2018-09-15 20:14:40 +0900

    DOM dependency fix
    https://github.com/trufflesuite/drizzle/pull/100
    autofix from eslint

--- src/Drizzle.js
+++ src/Drizzle.js
@@ -1,7 +1,10 @@
-// Load as promise so that async Drizzle initialization can still resolve
-var windowPromise = new Promise((resolve, reject) => {
-  window.addEventListener('load', resolve)
-})
+// Check to see if we are running in react-native.
+if (!(typeof navigator !== 'undefined' && navigator.product == 'ReactNative')) {
+  // Not running in react-native, load as promise so async Drizzle initialization still resolves
+  var windowPromise = new Promise((resolve, reject) => {
+    window.addEventListener('load', resolve)
+  })
+}
 
 class Drizzle {
   constructor (options, store) {
@@ -14,33 +17,31 @@ class Drizzle {
 
     this.loadingContract = {}
 
-    // Wait for window load event in case of injected web3.
-    windowPromise.then(() => {
-      // Begin Drizzle initialization.
-      store.dispatch({ type: 'DRIZZLE_INITIALIZING', drizzle: this, options })
-    })
+    // Check to see if we are running in react-native.
+    if (typeof navigator !== 'undefined' && navigator.product == 'ReactNative') {
+      // Running in react-native, no promise / event to wait for. Begin Drizzle initialization.
+      store.dispatch({type: 'DRIZZLE_INITIALIZING', drizzle: this, options})
+    } else {
+      // Not running in react-native, wait for window load event in case of injected web3.
+      windowPromise.then(() => {
+        // Begin Drizzle initialization.
+        store.dispatch({type: 'DRIZZLE_INITIALIZING', drizzle: this, options})
+      })
+    }
   }
 
   addContract (contractConfig, events = []) {
-    this.store.dispatch({
-      type: 'ADD_CONTRACT',
-      drizzle: this,
-      contractConfig,
-      events,
-      web3: this.web3
-    })
+    this.store.dispatch({type: 'ADD_CONTRACT', drizzle: this, contractConfig, events, web3: this.web3})
   }
 
   _addContract (drizzleContract) {
-    if (this.contracts[drizzleContract.contractName]) {
-      throw `Contract already exists: ${drizzleContract.contractName}`
-    }
+    if (this.contracts[drizzleContract.contractName]) { throw `Contract already exists: ${drizzleContract.contractName}` }
     this.contracts[drizzleContract.contractName] = drizzleContract
     this.contractList.push(drizzleContract)
   }
 
   findContractByAddress (address) {
-    return this.contractList.find(contract => {
+    return this.contractList.find((contract) => {
       return contract.address.toLowerCase() === address.toLowerCase()
     })
   }
